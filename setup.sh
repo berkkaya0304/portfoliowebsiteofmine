62c056857aa1df3380ffe5b39327229a17efa830
Berk Kaya <93735175+berkkaya0304@users.noreply.github.com>
2024-06-15 16:18:47 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 set -eEo pipefail
 
 if [ -z "$CLOUD_SHELL" ]; then
@@ -34,17 +20,7 @@ if [ -z "$CLOUD_SHELL" ]; then
 fi
 
 printf "Installing monolith dependencies...\n"
-cd ./monolith
-npm install
-printf "Completed.\n\n"
-
-printf "Installing microservices dependencies...\n"
-cd ../microservices
-npm install
-printf "Completed.\n\n"
-
-printf "Installing React app dependencies...\n"
-cd ../react-app
+cd ./portfoliowebsiteofmine
 npm install
 printf "Completed.\n\n"
 
